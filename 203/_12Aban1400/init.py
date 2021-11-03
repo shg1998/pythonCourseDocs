476ee86334d2c7ea8bda7a6f8e913f6a6f3410d0
shg 1998 <moh.nezh1377@gmail.com>
2021-11-03 12:46:26 +0330

    init.py 203 class has been added!

---
+++ 203/_12Aban1400/init.py
@@ -0,0 +1,44 @@
+# datatype = noe / ghaleb /zarf
+# str = رشته = > "salam"
+# int = عدد صحیح =>
+# float = عدد اعشاری
+
+# float
+# a = 25.5
+# print(type(a))
+
+# #int
+# b = 25
+# print(type(b))
+
+# #str
+# c = '25.5'
+# print(c)
+
+# dynamically type
+# ali = "Ali"
+# print(type(ali))
+# print("🌹🌹🌹🌹🌹")
+# ali = 2
+# print(type(ali))
+
+# concatenation
+# var1 = "salam"
+# var2 = "chetori?"
+# var3 = "manam khoobam"
+
+# print(var1, var2, var3)
+
+# print(var1+" "+var2+" "+var3)
+
+
+# sum of integer and string (using cast)
+# v1 = 17
+# v2 = "seyed"
+
+# print(v2+" "+str(v1))
+
+# , operation
+# num1 = 10
+# num2 = 11.5
+# print(num1,num2)
