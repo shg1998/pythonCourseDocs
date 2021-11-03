9dfcc6dde3da9b513a3a8eec31045ffb6fb755cf
shg 1998 <moh.nezh1377@gmail.com>
2021-11-03 11:15:26 +0330

    first Commit

---
+++ 201/_12Aban1400/init.py
@@ -0,0 +1,32 @@
+# DataType = Noe data / zarf / ghaleb
+
+# int => عدد صحیح
+# float => عدد اعشاری
+# str => رشته
+
+# concatenation
+adad1 = 1.6
+adad2 = "mohammad"
+print(str(adad1) + adad1)
+print("🌹🌹")
+
+# dynamically type
+a1 = "mohammad"
+print(a1)
+a1 = 123
+print(a1)
+print("🌹🌹")
+
+#sum of float and integer numbers
+va1 = 10
+va2 = 1.67
+sum = va1 + va2
+print(sum)
+print("🌹🌹")
+
+# get type of variable:
+print(type(va2))
+
+
+
+
---
+++ 202/_12Aban1400/main.py
@@ -0,0 +1,8 @@
+a = int(input("salam ye adad bede binim: "))
+b = int(input("salam ye adad dige bede binim: "))
+if a>b :
+    print (f"{a} is gt")
+elif a == b: 
+    print("shookhit gerefte!")
+else:
+    print(f"{b} is gt")
