7e5990225147ce6d223b42f7730771819e8487f0
shg 1998 <moh.nezh1377@gmail.com>
2021-11-10 08:57:45 +0330

    added 202 codes two samples!

---
+++ 202/_19Aban1400/Abs.py
@@ -0,0 +1,7 @@
+a = int(input("hello,please Enter a number : "))
+if a < 0 :
+    a = -1 * a
+else:
+    a = a
+
+print(a)
---
+++ 202/_19Aban1400/Odd_Even.py
@@ -0,0 +1,5 @@
+a = int(input("hello,please Enter a number : "))
+if a % 2 == 0:
+    print("your number is Even")
+else:
+    print("Your Number is Odd!")
