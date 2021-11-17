b69f06aad0f1c1b788e92822daa48a19556c9765
shg 1998 <moh.nezh1377@gmail.com>
2021-11-17 07:51:26 +0330

    19 aban docs Added!

---
+++ 203/_19Aban 1400/Abs.py
@@ -0,0 +1,12 @@
+a = int(input("Hello,please Enter a number: "))
+
+if a < 0:
+    a = -1 * a
+else:
+    a = a
+
+# print("absolute of this number is " + str(a))
+# print("ghadr motlag:", a)
+# print("ghadr motlagh : %d" % a)
+print(f"ghadr motlagh : {a}")
+# print(a)
---
+++ 203/_19Aban 1400/Odd_Even.py
@@ -0,0 +1,8 @@
+# print("Hello,please Enter a number: ")
+number = int(input("Hello,please Enter a number: "))
+
+if number % 2 == 0:
+    print("salam natije hast : ")
+    print("your number is Even")
+else:
+    print("your number is odd!")
