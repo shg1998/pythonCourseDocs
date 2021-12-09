d33eb36aa27c36d1b96742e3aee2dd359bd39110
shg 1998 <moh.nezh1377@gmail.com>
2021-12-09 10:03:43 +0330

    added 204 18 azar

---
+++ 204/_18Azar1400/age.py
@@ -0,0 +1,12 @@
+age = int(input("salam sennet ro bogo: "))
+
+if age > 90 : 
+    print("khoda omret bede")
+elif age > 50 :
+    print("bazneshaste")
+elif age > 25 : 
+    print("Ezdevaj kardi?")
+elif age> 19:
+    print("sarbazi che khabara")
+else:
+    print("bazito bokon")
