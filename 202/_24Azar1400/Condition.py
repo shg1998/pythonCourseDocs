bd802768cabd0010a3fb501d2a17034aa57e36b7
shg 1998 <moh.nezh1377@gmail.com>
2021-12-15 09:00:23 +0330

    24 azar 202 added

---
+++ 202/_24Azar1400/Condition.py
@@ -0,0 +1,19 @@
+
+# sample for learning Conditions : 
+
+age = int(input("salam sennet ro bego : "))
+
+if age >= 90 :
+    print("az shoma dige gozashte")
+elif age >= 50 :
+    print("Baaz neshaste!")
+elif age >= 30:
+    print("kasb o kar che khabara?")
+elif age >= 20:
+    print("sarbazi rafti?")
+elif age >= 7 : 
+    print("madrese cho khabar?")
+elif age <= 0 : 
+    print("khoobi bozorgvar?!!!!!!")
+else:
+    print("boro bazito bokon ... ")
---
+++ 202/_24Azar1400/sample2_Condition.py
@@ -0,0 +1,16 @@
+salary = int(input("Hello please Enter your monthly salary : "))
+
+# nested if 
+if salary >= 0:
+    if salary >= 70000000:
+        print("4 darsadi!")
+    elif salary >= 50000000:
+        print("baba khosh behalet!")
+    elif salary>=3000000:
+        print("hal mikonia!")
+    else:
+        print("khoda ziadesh kone!")
+elif salary >= -10 :
+    print("bedehi dari?")
+else:
+    print("dkddkhkdhkdh")
