6db37d5bff37d25d616d733ba1c6a1cb2215db55
shg 1998 <moh.nezh1377@gmail.com>
2022-02-09 10:57:09 +0330

    201 and 203 20 bahman

---
+++ 203/_20bahman1400/loop.py
@@ -0,0 +1,41 @@
+import random
+
+#  حلقه => تکرار ، پیمابش، چرخه 
+#  iterate
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# int float str bool
+
+col = [True,"mohammad",3,4,5.7]
+
+print(random.choice(col))
+print(col[int(random.random() * len(col))])
+
+# col.append(False)
+# col.remove(True)
+# col.clear()
+# len = col.count("salam")
+# col.reverse()
+# col.insert(0,"salam")
+
+
+# col.reverse()
+# col.append("salam")
+# col.reverse()
+
+
+# index = 0
+# # print(col)
+# for member in col:
+#     if index > 2 and index <5:
+#         print(member)
+#     index+=1
+
+
+# for i in col[2:5]:
+#     print(i)
