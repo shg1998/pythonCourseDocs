fdf7aa0a1381d043edc1e59267f0fd0bbe0e003b
shg 1998 <moh.nezh1377@gmail.com>
2022-03-02 08:56:22 +0330

    added 11 esfand 202

---
+++ 202/_11esfand1400/AdditionalFunction.py
@@ -0,0 +1,18 @@
+
+def sayHello(esm):
+    return f"salam {esm}!"
+
+def main():
+    name = input("say your name : ")
+    result = sayHello(name)
+    print(result)
+
+
+# main()
+
+# initial Arguments Value!
+# def printCountry(country="salam"):
+#     print("کشور تو  : ",country)
+
+# printCountry()
+
---
+++ 202/_11esfand1400/loop.py
@@ -0,0 +1,36 @@
+# what is Loop?!
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# a = range(5)
+# print(a)
+
+# for i in range(5):
+#     print(i+1)
+
+names = ["mohammad",3,"Mohsen",True,"Karim"]
+
+for i in range(len(names)):
+    print(names[i])
+
+# print(len(names))
+
+# print(names[5]) # out of range
+
+# names.clear()
+
+# names[2]  = 44.9875
+# names.append(33.67)
+# names.reverse()
+
+# names.reverse()
+# names.append("salam")
+# names.reverse()
+
+# names.insert(0,"salam")
+
+# print(names)
