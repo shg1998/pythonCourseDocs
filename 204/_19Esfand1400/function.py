9858bd1cca860adbc6190642ec54ada90ff1435b
shg 1998 <moh.nezh1377@gmail.com>
2022-03-10 09:22:08 +0330

    added 19 esfand 1400 for 204 class

---
+++ 204/_19Esfand1400/function.py
@@ -0,0 +1,19 @@
+
+# def Golabi(havij):
+#     result = (8 * (havij**2) - 3 * havij + 6)
+#     return result
+
+
+# x = int(input("please Enter x value : "))
+
+# kaftar = Golabi(x)
+
+# print("your result is : ", kaftar)
+
+
+def SayHello(esm = "Mohammad"):
+    print("salam ",esm)
+
+
+name = str(input("please Enter your Name : "))
+SayHello(name)
---
+++ 204/_19Esfand1400/list.py
@@ -0,0 +1,24 @@
+cars = ["BMW","Benz","Audi","Pride","peageut","porche","Pride","Pride"]
+# cars2 = list(("BMW","Benz","Audi","Pride","peageut","porche","Pride","Pride"))
+# print(cars[1])
+# cars[3] = "peykan"
+# cars.append("Lexus") # اضافه کردن به انتها
+# cars.remove("Pride")
+# del cars[3]
+# print(cars[3:-1])
+# print(cars.count("Pride"))
+
+# print(cars[:])
+# print(cars[:4])
+# print(cars[2:])
+
+# thislist = ["apple", "banana", "cherry"]
+
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
